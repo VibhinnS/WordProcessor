68207eb8859f1322d4dc7b56668d21a5b5f0af01
VibhinnS <121715743+VibhinnS@users.noreply.github.com>
2023-02-05 22:43:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function boldText() {
+
+    document.execCommand('bold');
+    
+}
+
+function italicText() {
+
+    document.execCommand('italic');
+
+}
+
+
+function underlineText() {
+
+    document.execCommand('underline');
+
+}
+
+
+
